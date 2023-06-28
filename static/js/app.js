a1ccfaa51dea19c779f903774061dda6e37dba25
Kesha Patel <keshakp@gmail.com>
2023-06-28 18:34:32 -0400

    Final Challenge 14 submission

--- static/js/app.js
+++ static/js/app.js
@@ -3,15 +3,15 @@
 
 const sample_json = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-d3.json(sample_json).then(function(data) {
+d3.json(sample_json).then(function (data) {
     console.log("Sample")
     console.log(data);
 });
 
 
-function bellybuttonchart(iteam){
+function bellybuttonchart(iteam) {
     console.log("bellybuttonchart");
-    d3.json(sample_json).then(function(data) {
+    d3.json(sample_json).then(function (data) {
         console.log("Bellybuttonsample")
         console.log(data);
 
@@ -22,14 +22,14 @@ function bellybuttonchart(iteam){
         console.log(samplelist);
 
         var sampleoutcome = samplelist[0];
-        console.log (sampleoutcome)
+        console.log(sampleoutcome)
 
         var sample_values = sampleoutcome.sample_values;
-        console.log ("sample_values")
+        console.log("sample_values")
         console.log(sample_values);
 
         var OTUs = sampleoutcome.otu_ids;
-        console.log ("otu_ids")
+        console.log("otu_ids")
         console.log(OTUs);
 
         var otu_lables = sampleoutcome.otu_labels;
@@ -45,61 +45,61 @@ function bellybuttonchart(iteam){
 
 
         // from the URL 
-        
+
         var bar = [{
-            x: sample_values.slice(0,10).reverse(),
-            y: OTUs.slice(0,10).map(ID => `OTU ${ID}`).reverse(),
-            text: otu_lables.slice(0,10).reverse(),
+            x: sample_values.slice(0, 10).reverse(),
+            y: OTUs.slice(0, 10).map(ID => `OTU ${ID}`).reverse(),
+            text: otu_lables.slice(0, 10).reverse(),
             type: 'bar',
             orientation: 'h'
         }];
 
         Plotly.newPlot('bar', bar);
 
-         // 3. Create a bubble chart that displays each sample.
-            // Use otu_ids for the x values.
-            // Use sample_values for the y values.
-            // Use sample_values for the marker size.
-            // Use otu_ids for the marker colors.
-            // Use otu_labels for the text value
+        // 3. Create a bubble chart that displays each sample.
+        // Use otu_ids for the x values.
+        // Use sample_values for the y values.
+        // Use sample_values for the marker size.
+        // Use otu_ids for the marker colors.
+        // Use otu_labels for the text value
 
         var Bubble = {
             x: OTUs,
             y: sample_values,
             mode: 'markers',
-            marker: {size: sample_values,color: OTUs,colorscale: 'YlGnBu'}
-    }
-     
-    
+            marker: { size: sample_values, color: OTUs, colorscale: 'YlGnBu' }
+        }
+
+
         var BubbleData = [Bubble];
-    
+
         var BubbleLayout = {
             titiel: "Bacteria",
             showlegend: false,
-            xaxis: {titiel: "otu_ids"},
-            yaxis: {titiel: "sample_values"}
-    
-    }
-    
-        Plotly.newPlot('bubble', BubbleData, BubbleLayout );
-    
+            xaxis: { titiel: "otu_ids" },
+            yaxis: { titiel: "sample_values" }
+
+        }
+
+        Plotly.newPlot('bubble', BubbleData, BubbleLayout);
+
 
     });
 };
 
-    bellybuttonchart(940);
+bellybuttonchart(940);
 
 
 // 4. Display the sample metadata, i.e., an individual's demographic information.
 
-function Metadatalist(iteam){
+function Metadatalist(iteam) {
 
-    d3.json(sample_json).then (function(data) {
+    d3.json(sample_json).then(function (data) {
         console.log("Metadata Samples")
         console.log(data)
 
         var metadata = data.metadata;
-        console.log ("MetadataArray");
+        console.log("MetadataArray");
         console.log(metadata)
 
         var MDArray = metadata.filter(MDObject => MDObject.id == iteam)
@@ -109,55 +109,62 @@ function Metadatalist(iteam){
         console.log("metadata result");
         console.log(MDResult)
 
-// 5. Display each key-value pair from the metadata JSON object somewhere on the page.
+        // 5. Display each key-value pair from the metadata JSON object somewhere on the page.
 
-//     ![hw](https://static.bc-edx.com/data/dl-1-2/m14/lms/img/hw03.jpg)
+        //     ![hw](https://static.bc-edx.com/data/dl-1-2/m14/lms/img/hw03.jpg)
 
-       
+
         var MDPenal = d3.select("#sample-metadata");
 
         MDPenal.html("");
 
+        // for (key in MDResult) {
+        //     Object.entries(MDResult).forEach(([key, value]) => { 
+        //        MDPenal.append("h3").text(`${key}: ${value}`) });
         for (key in MDResult) {
-            Object.entries(MDResult).forEach(([key, value]) => { 
-               MDPenal.append("h3").text(`${key}: ${value}`) });
 
-        
-}});
-};
-
-    Metadatalist(940)
+            MDPenal.append("h3").text(`${key}: ${MDResult[key]}`);
+        };
 
 
-// 6. Update all the plots when a new sample is selected. Additionally, you are welcome to create any layout that you would like for your dashboard. An example dashboard is shown as follows:
+    });
+}
+    //     });
+    // };
 
-//     ![hw](https://static.bc-edx.com/data/dl-1-2/m14/lms/img/hw02.jpg)
+    Metadatalist(940);
 
-function optionChanged(NewSample) {
+
+    // 6. Update all the plots when a new sample is selected. Additionally, you are welcome to create any layout that you would like for your dashboard. An example dashboard is shown as follows:
+
+    //     ![hw](https://static.bc-edx.com/data/dl-1-2/m14/lms/img/hw02.jpg)
+
+    function optionChanged(NewSample) {
         bellybuttonchart(NewSample);
         Metadatalist(NewSample);
     };
-    
-function initial() {
-     d3.json(sample_json).then(function (data){
 
-        
-        var NewSample = data.names;
-        console.log("NewSample")
-        console.log(NewSample);
-        
+    function initial() {
+        d3.json(sample_json).then(function (data) {
 
-        var dropdown = d3.select("#selDataset");
 
-        for (let index = 0; index < NewSample.length; index++) {
-            dropdown.append("option").property("value", NewSample[index]).text(NewSample[index])};
-        
-        let initialsample = NewSample[0];
-        bellybuttonchart(initialsample);
-        Metadatalist(initialsample);
-    });
-        
-};
+            var NewSample = data.names;
+            console.log("NewSample")
+            console.log(NewSample);
+
+
+            var dropdown = d3.select("#selDataset");
+
+            for (let index = 0; index < NewSample.length; index++) {
+                dropdown.append("option").property("value", NewSample[index]).text(NewSample[index])
+            };
+
+            let initialsample = NewSample[0];
+            bellybuttonchart(initialsample);
+            Metadatalist(initialsample);
+        });
+
+    };
 
     initial(940);
 
